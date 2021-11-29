38e6d643e1f09f184d94af2fc6d2ecde84f4b78a
tuannguyenviet <93248379+tuannguyenviet@users.noreply.github.com>
2021-11-29 15:27:28 +0700

    Add files via upload

---
+++ App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace T2010A_WAD
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
